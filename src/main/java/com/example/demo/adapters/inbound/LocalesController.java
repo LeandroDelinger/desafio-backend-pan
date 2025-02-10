7d015abce79d28c059a16250b5e238eb6d4ffe93
Leandro Delinger <leandro.delinger@gmail.com>
2025-02-10 00:12:34 -0300

    feature: implementing logs to Locales Jorney

--- src/main/java/com/example/demo/adapters/inbound/LocalesController.java
+++ src/main/java/com/example/demo/adapters/inbound/LocalesController.java
@@ -1,9 +1,12 @@
 package com.example.demo.adapters.inbound;
 
+import com.example.demo.adapters.outbound.repository.ClientRepositoryImpl;
 import com.example.demo.application.core.locales.Cep;
 import com.example.demo.application.core.locales.Municipality;
 import com.example.demo.application.core.locales.State;
 import com.example.demo.application.ports.in.LocaleServicePort;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,7 @@ import java.util.List;
 @RequestMapping("/locale")
 public class LocalesController {
     private final LocaleServicePort localeServicePort;
+    private static final Logger log = LoggerFactory.getLogger(LocalesController.class);
 
     public LocalesController(LocaleServicePort localeServicePort) {
         this.localeServicePort = localeServicePort;
@@ -22,16 +26,19 @@ public class LocalesController {
 
     @GetMapping("/address/{cep}")
     public Cep getClientByCPF(@PathVariable String cep) {
+        log.info("Received request to get locale by CEP: {}", cep);
         return localeServicePort.getLocaleByCep(cep);
     }
 
     @GetMapping("/states")
     public List<State> getStates() {
+        log.info("Received request to get all states");
         return localeServicePort.getStates();
     }
 
     @GetMapping("/municipalities/{stateId}")
     public List<Municipality> getMunicipalitiesByStateID(@PathVariable long stateId) {
+        log.info("Received request to get municipalities by state ID: {}", stateId);
         return localeServicePort.getMunicipalitiesByStateID(stateId);
     }
 }
--- src/main/java/com/example/demo/adapters/outbound/rest/SearchLocalesAdapter.java
+++ src/main/java/com/example/demo/adapters/outbound/rest/SearchLocalesAdapter.java
@@ -3,11 +3,14 @@ package com.example.demo.adapters.outbound.rest;
 import com.example.demo.application.core.locales.Cep;
 import com.example.demo.application.core.locales.Municipality;
 import com.example.demo.application.core.locales.State;
+import com.example.demo.application.core.service.LocaleServiceImpl;
 import com.example.demo.application.ports.out.SearchLocalesPort;
 import com.example.demo.utils.mappers.CepMapper;
 import com.example.demo.utils.mappers.MunicipalityMapper;
 import com.example.demo.utils.mappers.StateMapper;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -16,6 +19,7 @@ import java.util.Optional;
 @Component
 @RequiredArgsConstructor
 public class SearchLocalesAdapter implements SearchLocalesPort {
+    private static final Logger log = LoggerFactory.getLogger(SearchLocalesAdapter.class);
     private final CepMapper cepMapper;
     private final StateMapper stateMapper;
     private final MunicipalityMapper municipalityMapper;
@@ -23,6 +27,7 @@ public class SearchLocalesAdapter implements SearchLocalesPort {
 
     @Override
     public Optional<Cep> searchAddressWithCEP(String cep) {
+        log.info("Searching address with CEP: {}", cep);
         return localesApiClient.getAddress(cep)
                 .filter(cepDTO -> cepDTO.getCep() != null && !cepDTO.getCep().isBlank())
                 .map(cepMapper::DTOtoDomain);
@@ -30,11 +35,23 @@ public class SearchLocalesAdapter implements SearchLocalesPort {
 
     @Override
     public List<State> getStates() {
-        return localesApiClient.getStates().stream().map(stateMapper::DTOtoDomain).toList();
+        log.info("Fetching list of states.");
+        List<State> states = localesApiClient.getStates().stream()
+                .map(stateMapper::DTOtoDomain)
+                .toList();
+
+        log.info("Fetched {} states.", states.size());
+        return states;
     }
 
     @Override
     public List<Municipality> getMunicipalitiesByStateID(long stateId) {
-        return localesApiClient.getMunicipalitiesByStateID(stateId).stream().map(municipalityMapper::DTOtoDomain).toList();
+        log.info("Fetching municipalities for state ID: {}", stateId);
+        List<Municipality> municipalities = localesApiClient.getMunicipalitiesByStateID(stateId).stream()
+                .map(municipalityMapper::DTOtoDomain)
+                .toList();
+
+        log.info("Fetched {} municipalities for state ID: {}", municipalities.size(), stateId);
+        return municipalities;
     }
 }

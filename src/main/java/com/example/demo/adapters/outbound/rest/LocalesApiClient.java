ac31fa7bab0d17b5a2fd9ac1567b133847c24a27
Leandro Delinger <leandro.delinger@gmail.com>
2025-02-10 19:18:07 -0300

    refactor: removing urls from the code

--- src/main/java/com/example/demo/adapters/outbound/rest/LocalesApiClient.java
+++ src/main/java/com/example/demo/adapters/outbound/rest/LocalesApiClient.java
@@ -3,10 +3,10 @@ package com.example.demo.adapters.outbound.rest;
 import com.example.demo.adapters.outbound.dto.CepResponseDTO;
 import com.example.demo.adapters.outbound.dto.MunicipalityFullDTO;
 import com.example.demo.adapters.outbound.dto.StateResponseDTO;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,13 +18,22 @@ public class LocalesApiClient {
     private final RestTemplate restTemplate;
     private static final Logger log = LoggerFactory.getLogger(LocalesApiClient.class);
 
+    @Value("${api.viacep.url}")
+    private String viaCepUrl;
+
+    @Value("${api.ibge.states.url}")
+    private String ibgeStatesUrl;
+
+    @Value("${api.ibge.municipalities.url}")
+    private String ibgeMunicipalitiesUrl;
+
     @Autowired
     public LocalesApiClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     public Optional<CepResponseDTO> getAddress(String cep) {
-        String url = String.format("https://viacep.com.br/ws/%s/json", cep);
+        String url = String.format("%s%s/json", viaCepUrl, cep);
         try {
             CepResponseDTO response = restTemplate.getForObject(url, CepResponseDTO.class);
             return Optional.of(response);
@@ -35,9 +44,8 @@ public class LocalesApiClient {
     }
 
     public List<StateResponseDTO> getStates() {
-        String url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/";
         try {
-            StateResponseDTO[] response = restTemplate.getForObject(url, StateResponseDTO[].class);
+            StateResponseDTO[] response = restTemplate.getForObject(ibgeStatesUrl, StateResponseDTO[].class);
             return List.of(response);
         } catch (Exception e) {
             log.error("Error occurred while fetching states", e);
@@ -47,7 +55,7 @@ public class LocalesApiClient {
     }
 
     public List<MunicipalityFullDTO> getMunicipalitiesByStateID(long stateId) {
-        String url =  String.format("https://servicodados.ibge.gov.br/api/v1/localidades/estados/%d/municipios", stateId);
+        String url = ibgeMunicipalitiesUrl.replace("{stateId}", String.valueOf(stateId));
         try {
             MunicipalityFullDTO[] response = restTemplate.getForObject(url, MunicipalityFullDTO[].class);
             return List.of(response);
